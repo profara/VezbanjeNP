dcf4614bfb2ba96f7f6796b50778b26d00c1a5cd
Aleksandar Markovic <alekbg99@gmail.com>
2023-04-03 23:17:46 +0200

    Dodate hashCode, equals i toString metode u klase Knjiga i Autor

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -1,5 +1,73 @@
 package biblioteka;
 
+import java.util.Objects;
+
 public class Autor {
+	
+	private String JMBG;
+	private String ime;
+	private String prezime;
+	
+	public Autor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Autor(String jMBG, String ime, String prezime) {
+		super();
+		JMBG = jMBG;
+		this.ime = ime;
+		this.prezime = prezime;
+	}
+
+	public String getJMBG() {
+		return JMBG;
+	}
+
+	public void setJMBG(String jMBG) {
+		JMBG = jMBG;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(JMBG);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return Objects.equals(JMBG, other.JMBG);
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [JMBG=" + JMBG + ", ime=" + ime + ", prezime=" + prezime + "]";
+	}
+	
+	
+	
+	
+
 
 }
