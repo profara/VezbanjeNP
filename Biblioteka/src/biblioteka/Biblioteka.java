55f9a9a51884147646da866fa8f0e8d88e5f60ce
Aleksandar Markovic <alekbg99@gmail.com>
2023-04-05 12:28:11 +0200

    Dodata klasa Biblioteka

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,43 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterface;
+
+public class Biblioteka implements BibliotekaInterface {
+
+	private List<Knjiga> knjige;
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		// TODO Auto-generated method stub
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		List<Knjiga> rezultati = new ArrayList<>();
+
+		for (Knjiga k : knjige) {
+			if (k.getNaslov().toLowerCase().contains(naslov.toLowerCase().trim())) {
+				rezultati.add(k);
+			}
+		}
+
+		return rezultati;
+	}
+
+}
